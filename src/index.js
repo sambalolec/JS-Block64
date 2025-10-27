cf57ef1a64dc35ea85d62ab3853cf690903fdc37
Hans-Juergen Grauer <Hans-Juergen.Grauer@dci-student.org>
2025-10-27 15:25:50 +0100

    Ausgabe jetzt in Hex, mehr kommentiert, kleiner Bug gefixt

--- src/index.js
+++ src/index.js
@@ -345,34 +345,38 @@ function decrypt(blocks, passphrase) {
 
 //**************************************  UI and Interaction  **************************************//
 
-const outputEl = document.getElementById("output");
+const output = document.getElementById("output");
 
 // Hilfsfunktion um Console und Output zu bedienen
 function log(...args) {
   console.log(...args);
-  outputEl.textContent += args.join(" ") + "\n";
+  output.textContent += args.join(" ") + "\n";
 }
 
 userInput = {};
 
 // Event listener für "Run" button
 document.getElementById("runBtn").addEventListener("click", () => {
-  outputEl.textContent = ""; // Vor jedem Lauf resetten
+  output.textContent = ""; // Vor jedem Lauf resetten, sonst Layout kaputt.
 
-  // --- Eingaben einlesen ---
+  // Eingaben einlesen
   userInput.value = document.getElementById("plaintext").value;
   log("Original:", userInput.value);
   const plainkey = document.getElementById("plainkey").value;
   log("Passwort:", plainkey);
 
   // --- Hauptprogramm ---
+
+  // Objekt "userInput" verschlüsseln -> "chiffrat"
   const chiffrat = encrypt(userInput, plainkey);
+
+  // Format: Ausgabe in Hex mit führenden Nullen; Accent grave ausprobiert.
   log(
-    "Encrypted Blocks:",
-    // chiffrat.map((x) => "0x" + x.toString(16)) // Format: Ausgabe in Hex
-    chiffrat.map((x) => "0x" + x.toString(16).padStart(16, "0")) // Ausgabe in Hex inklusive führender Nullen (holy shit)
+    `Encrypted Blocks ${chiffrat.length}:`,
+    chiffrat.map((x) => "0x" + x.toString(16).padStart(16, "0"))
   );
 
+  // "chiffrat" wieder entschlüsseln -> "result"
   const result = decrypt(chiffrat, plainkey);
   log("Decrypted:", result.value);
 });
